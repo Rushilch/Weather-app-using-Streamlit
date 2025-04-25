c45e2c09de9da9eaf02da7a94bab4131fa50e656
Rushil Chilakamarri <155135792+Rushilch@users.noreply.github.com>
2025-04-25 10:53:19 +0530

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
 def get_api():
-    return "c176203b80094939898102242240609"
+    return API_KEY
 
 
 if __name__ == "__main__":
-    df = pd.read_json("http://api.weatherapi.com/v1/forecast.json?key=c176203b80094939898102242240609&q=london")
+    df = pd.read_json("http://api.weatherapi.com/v1/forecast.json?key={API_KEY}&q=london")
     print(df)
